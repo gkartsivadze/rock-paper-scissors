c5bc09301491909bc275f7b6dbf521c90ce22809
gkartsivadze <qarcigio444@mail.ru>
2023-09-17 22:43:27 +0400

    tiny improvments

--- src/components/Choose.jsx
+++ src/components/Choose.jsx
@@ -1,14 +1,13 @@
 import ChooseButton from "./ChooseButton";
+import pentagonImg from "../assets/pentagon.svg"
 
 export default function Choose({ variants }) {
     return (
         <main id="choose" className=" row-span-3 relative">
             { variants.map(variant => <ChooseButton key={variant.id} variant={variant} />) }
-            <svg
-                className=" absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
-                viewBox="0 0 300 300"
-                width="300" height="300">
-            </svg>
+            <img
+                src={pentagonImg}
+                alt="" />
         </main>
     )
 };
