bdfdd38bb47fe6b535a63a30ae0057311ab9b53b
gkartsivadze <qarcigio444@mail.ru>
2023-09-18 23:38:23 +0400

    Minified code

--- src/components/Choose.jsx
+++ src/components/Choose.jsx
@@ -5,9 +5,9 @@ export default function Choose({ variants }) {
     return (
         <main id="choose" className=" row-span-3 relative">
             { variants.map(variant => <ChooseButton key={variant.id} variant={variant} />) }
-            <img
+            {/* <img
                 src={pentagonImg}
-                alt="" />
+                alt="" /> */}
         </main>
     )
 };
