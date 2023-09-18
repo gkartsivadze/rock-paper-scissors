f109ea4b0dc0369e86321401314d5924f0b8534e
gkartsivadze <qarcigio444@mail.ru>
2023-09-18 23:49:07 +0400

    optimized svg

--- src/assets/Pentagon.jsx
+++ src/assets/Pentagon.jsx
@@ -1,23 +1,8 @@
 export default function Pentagon({ ...props }) {
 	return (
+		<svg { ...props } xmlns="http://www.w3.org/2000/svg" width="350" height="350" viewBox="0 0 76.548 76.548">
+  <path d="M61.929 74.675H14.619L0 29.68 38.274 1.873l38.274 27.808L61.929 74.675zM16.799 71.675h42.95L73.021 30.826 38.274 5.582 3.526 30.827 16.799 71.675z" stroke-width="2" />
+</svg>
 
-		<svg
-			{...props}
-			fill="#000000"
-			xmlns="http://www.w3.org/2000/svg"
-			xmlns: xlink="http://www.w3.org/1999/xlink"
-			width="350px"
-			height="350px"
-			viewBox="0 0 76.548 76.548"
-			xml: space="preserve">
-			<g>
-				<g>
-					<path d="M61.929,74.675h-47.31L0,29.68L38.274,1.873l38.274,27.808L61.929,74.675z M16.799,71.675h42.95l13.272-40.849
-			L38.274,5.582L3.526,30.827L16.799,71.675z"/>
-				</g>
-			</g>
-		</svg>
 	)
 };
-
-
